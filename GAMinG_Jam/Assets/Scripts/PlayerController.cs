5b12bdb21a71a00994f8d418f47278cd64ce7b13
MatheusCapucho <matheusfcapucho@gmail.com>
2020-07-28 19:06:14 -0300

    [SETUP] Sample Scene

---
+++ GAMinG_Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{   
+    public Rigidbody2D rb;
+    public Camera cam;
+    
+    private  Vector2 movementInput;
+    private Vector2 mousePosition;
+    private float speed = 10f;
+   
+    private void Awake()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();   
+    }
+
+    void Update()
+    {       
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+    private void FixedUpdate() {
+
+        Vector2 lookAtMouse = mousePosition - rb.position;
+
+        rb.velocity = movementInput * speed;
+    }
+}
